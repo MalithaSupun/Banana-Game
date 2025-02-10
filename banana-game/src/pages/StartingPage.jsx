07399c587dc4d3c41164db6668d562846562bb57
Malitha Supun <malithasupun2@gmail.com>
2025-02-10 22:51:00 +0530

    refactor: simplify StartingPage layout and enhance Tailwind configuration with new colors and font

--- banana-game/src/pages/StartingPage.jsx
+++ banana-game/src/pages/StartingPage.jsx
@@ -3,16 +3,9 @@ import startingPageImg from "../assets/background_img.png";
 
 export default function StartingPage() {
   return (
-    <div className="relative flex items-center justify-center h-screen bg-gray-900">
-      {/* Background Image */}
-      <img src={startingPageImg} alt="Starting Page" className="w-full h-full object-cover absolute top-0 left-0" />
-      
-      {/* Container for Text */}
-      <div className="absolute text-center">
-        <h1 className="text-5xl font-bold text-white bg-pink-400 px-6 py-2 rounded-lg">
-          Banana Maths
-        </h1>
-      </div>
+    <div className="flex items-center justify-center h-screen bg-gray-900">
+      <img src={startingPageImg} alt="Starting Page" className="w-full h-full object-cover" />
+
     </div>
   );
 }
--- banana-game/tailwind.config.js
+++ banana-game/tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+    'primary': '#EEC4BE',
+    'secondary': '#72D772',
+    'thirdcolor': '#B3F3B3',
+    'fourthcolor': '#FCF7DD'
+  },
+  fontFamily: {
+        dancingScript: ['Dancing Script', 'cursive'], // Added Dancing Script font
+      },
+    },
   },
   plugins: [],
 }
