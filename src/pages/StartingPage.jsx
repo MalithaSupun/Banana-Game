bc1b2dbd2f91008c20cfbc4ea1deb6a764a86b79
Malitha Supun <malithasupun2@gmail.com>
2025-02-10 23:39:19 +0530

    add images to StartingPage for enhanced visual appeal

--- src/pages/StartingPage.jsx
+++ src/pages/StartingPage.jsx
@@ -3,6 +3,8 @@ import startingPageImg from "../assets/background_img.png";
 import BananaTitleBox from '../components/BananaTitleBox';
 import MathFunStartBox from "../components/MathFunStartBox";
 import Loading from "../components/Loading";
+import banana from "../assets/Startingpageimg2.png";
+import bananslices from "../assets/Startingpageimg3.png"
 
 export default function StartingPage() {
   return (
@@ -13,6 +15,17 @@ export default function StartingPage() {
         <MathFunStartBox />
         <Loading />
       </div>
+      {/* Image positioned at the bottom-right */}
+      <img 
+        src={banana} 
+        alt="Bottom Right Image" 
+        className="absolute bottom-0 right-0 max-w-96 max-h-96 object-cover mb-4 mr-4" 
+      />
+      <img 
+        src={bananslices} 
+        alt="Bottom Right Image" 
+        className="absolute bottom-0 left-0 max-w-96 max-h-96 object-cover mb-20 mr-20" 
+      />
     </div>
   );
 }
