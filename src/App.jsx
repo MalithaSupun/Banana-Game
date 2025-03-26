7edae862210594ce03a1955d95ffd8b386770ef1
Malitha Supun <malithasupun2@gmail.com>
2025-03-26 13:09:25 +0530

    enhance(App); switch from BrowserRouter to HashRouter for better URL handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import StartingPage from "./pages/StartingPage";
 import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/LoginPage";
